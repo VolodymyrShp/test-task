e9d3b1944f2a3abb5b87b97bb77f33c8a5fd9fce
VolodymyrShp <vladimir.shpachuk@gmail.com>
2023-01-15 19:19:54 +0200

    last

---
+++ client/src/views/deleteUser.vue
@@ -0,0 +1,28 @@
+<script >
+import axios from 'axios'
+import { useRoute, useRouter } from 'vue-router';
+const router = useRouter()
+const route = useRoute()
+export default {
+    methods: {
+        userDelete() {
+            let result = axios.delete(`http://localhost:8080/user/delete/${this.$route.params.id}`)
+            if (this.$route.params.id) { this.$router.push({ path: '/' }) }
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <div class="popup__delete-confirm">
+        <div class="popup__inner">
+            <p>Are you sure</p>
+            <hr>
+            <button class="close" @click="$router.push('/')">No</button>
+            <button v-on:click="userDelete" class="yes">Yes</button>
+        </div>
+    </div>
+
+</template>
--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 
 
 app.use(cors({
-    origin: '*',
+    origin: '127.0.0.1"5500',
 }));
 
 
